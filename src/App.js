2078f6d13b71e5e36fd3a94bcd720eb1c014117d
Jaeheon Shim <git@jaeheonshim.com>
2022-08-12 00:03:57 -0400

    Add PWA serviceworker

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import LoginForm from './Login/LoginForm';
 import { createContext, useEffect, useState } from 'react';
-import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
+import { HashRouter, Route, Routes, Navigate } from 'react-router-dom';
 import Login from './pages/Login';
 import Home from './pages/Home';
 import Footer from './Footer';
@@ -16,12 +16,12 @@ function App() {
 
   return (
     <AppContext.Provider value={{appState: appState, setAppState: setAppState}}>
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/" element={!appState.id || !appState.password ? <Navigate to="/login" /> : <Home />} />
           <Route path="login" element={appState.id || appState.password ? <Navigate to="/" /> : <Login />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
       <Footer />
     </AppContext.Provider>
   );
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
 import reportWebVitals from './reportWebVitals';
+import * as serviceWorker from './serviceWorker';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -14,3 +15,4 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+serviceWorker.register();
