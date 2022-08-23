196de3d4971e6475a009f98c624f8c4cddeb1d65
Jaeheon Shim <git@jaeheonshim.com>
2022-08-23 17:49:16 -0400

    Add reset to edited grades

--- src/Home/Assignment.js
+++ src/Home/Assignment.js
@@ -1,4 +1,5 @@
-import { Row, Col } from "react-bootstrap"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Row, Col, Button } from "react-bootstrap"
 import { useForceUpdate } from "../util/hooks";
 
 export default function Assignment(props) {
@@ -9,6 +10,11 @@ export default function Assignment(props) {
         props.forceUpdate();
     }
 
+    const reset = () => {
+        props.assignment.modifiedScore = /^\+?(0|[1-9]\d*)$/.test(props.assignment.Score.split(" ")[0]) ? parseInt(props.assignment.Score.split(" ")[0]) : -1;
+        props.forceUpdate();
+    }
+
     return (
         <div>
             <Row>
@@ -16,10 +22,13 @@ export default function Assignment(props) {
                     <h3 className="h6 fw-semibold mb-0">{props.Measure}</h3>
                     <div style={{fontSize: "0.7em", lineHeight: 1.4}} className="mt-1 text-secondary">{props.Course ? props.Course + " | " : ""} {props.Type} | {props.Date}</div>
                 </Col>
-                <Col>
+                <Col className="d-flex justify-content-end gap-2">
                     {props.edit ? 
-                        <input type="number" className="float-end" style={{width: "5em"}} onChange={onEditChange} value={props.assignment.modifiedScore} /> :
-                        <span className="display-6 float-end">{/^\+?(0|[1-9]\d*)$/.test(props.Score.split(" ")[0]) ? props.Score.split(" ")[0] : "--"}</span>
+                        <>
+                            <Button onClick={reset} size="sm" variant="outline-secondary"><FontAwesomeIcon icon="fas fa-undo" /></Button>
+                            <input type="number" className="float-end" style={{width: "5em"}} onChange={onEditChange} value={props.assignment.modifiedScore} />
+                        </> :
+                        <div className="display-6 align-self-end">{/^\+?(0|[1-9]\d*)$/.test(props.Score.split(" ")[0]) ? props.Score.split(" ")[0] : "--"}</div>
                     }
                 </Col>
             </Row>
--- src/Home/CourseDisplay.js
+++ src/Home/CourseDisplay.js
@@ -2,6 +2,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useEffect } from "react";
 import { Button } from "react-bootstrap";
 import { useForceUpdate } from "../util/hooks";
+import Assignment from "./Assignment";
 import AssignmentList from "./AssignmentList";
 import GradeSummary from "./GradeSummary";
 
@@ -22,15 +23,26 @@ export default function CourseDisplay(props) {
         return Math.round((weightedNumerator / weightedDenominator) * 10) / 10;
     }
 
+    const resetAll = () => {
+        for(const assignment of props.Mark.Assignments) {
+            assignment.modifiedScore = /^\+?(0|[1-9]\d*)$/.test(assignment.Score.split(" ")[0]) ? parseInt(assignment.Score.split(" ")[0]) : -1;
+        }
+
+        forceUpdate();
+    }
+
     return (
         <div className="pb-5">
             <div className="d-flex gap-2">
                 <Button onClick={props.onClose} variant="secondary" className="mb-2" size="md"><FontAwesomeIcon icon="fa-solid fa-angle-left" /></Button>
                 <GradeSummary edit={props.edit} Period={props.Course.Period} Title={props.Course.Title} Room={props.Course.Room} Staff={props.Course.Staff} CalculatedScoreString={props.edit ? calculateEditedScore() : props.Mark.CalculatedScoreString} />
             </div>
-            <div className="d-flex mt-4 mb-2">
+            <div className="d-flex mt-4 mb-2 justify-content-between">
                 <h6 className="fw-semibold mt-0 align-self-center">Assignments ({props.Mark.Assignments.length})</h6>
-                <Button onClick={props.toggleEdit} style={{ marginLeft: "auto" }} variant={props.edit ? "success" : "outline-secondary"} size="sm" active={props.edit}><FontAwesomeIcon icon="fa-solid fa-calculator" /></Button>
+                <div className="d-flex gap-1">
+                    <Button onClick={resetAll} style={{display: props.edit ? "block" : "none"}} size="sm" variant="secondary"><FontAwesomeIcon icon="fas fa-undo" /></Button>
+                    <Button onClick={props.toggleEdit} variant={props.edit ? "success" : "outline-secondary"} size="sm" active={props.edit}><FontAwesomeIcon icon="fa-solid fa-calculator" /></Button>
+                </div>
             </div>
             <hr className="mt-0" />
             <AssignmentList forceUpdate={forceUpdate} edit={props.edit} Assignments={props.Mark.Assignments} />
